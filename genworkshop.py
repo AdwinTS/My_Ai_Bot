5b7309894c47123ce0ddad5258ea759649924d6b
Adwin T Sunil <145273482+AdwinTS@users.noreply.github.com>
2025-03-10 22:48:44 +0530

    Add files via upload

---
+++ genworkshop.py
@@ -0,0 +1,18 @@
+
+
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="who is owner of isl"
+)
+
+print(response.text)
+
